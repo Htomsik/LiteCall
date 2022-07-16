e88b7f0ca74bfbd2d78241f3fd7ae1bdd78e3dcb
Константин Несутулов <186141@edu.fa.ru>
2022-07-17 00:58:58 +0300

    Hide ApiKey

--- Core/Stores/Connections/HttpClientStore.cs
+++ Core/Stores/Connections/HttpClientStore.cs
@@ -1,13 +1,13 @@
 ﻿using Core.VMD.Base;
+using Microsoft.Extensions.Configuration;
 
 namespace Core.Stores.Connections;
 
 public class HttpClientStore : BaseVmd
 {
-    private const string ApiKey = "ACbaAS324hnaASD324bzZwq41";
     public readonly HttpClient CurrentHttpClient;
 
-    public HttpClientStore()
+    public HttpClientStore(IConfiguration configuration)
     {
         var clientHandler = new HttpClientHandler
         {
@@ -18,7 +18,7 @@ public class HttpClientStore : BaseVmd
         CurrentHttpClient = new HttpClient(clientHandler)
         {
             Timeout = TimeSpan.FromSeconds(10),
-            DefaultRequestHeaders = { { "ApiKey", ApiKey } }
+            DefaultRequestHeaders = { { "ApiKey", configuration["ApiKey"] } }
         };
     }
 }
