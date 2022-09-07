5f8bb8799596154f01586f548ce20a30b0484544
JessJake <wont.miner@gmail.com>
2022-09-07 17:32:28 +0300

    Add BaseReactiveDateStore.cs

---
+++ Core/Stores/Base/BaseReactiveDateStore.cs
@@ -0,0 +1,26 @@
+﻿using AppInfrastructure.Stores.DefaultStore;
+using DynamicData.Binding;
+using ReactiveUI;
+
+namespace Core.Stores.Base;
+
+/// <summary>
+///     Base store for reactive data
+/// </summary>
+public class BaseReactiveDateStore<TValue> : BaseLazyStore<TValue> where TValue : IReactiveObject
+{
+   public override TValue? CurrentValue
+   {
+       get => (TValue?)_currentValue.Value;
+       set
+       {
+           _currentValue = new Lazy<object?>(()=> value);
+            
+           CurrentValue?.WhenAnyPropertyChanged()
+               .Subscribe(_ => OnCurrentValueChanged());
+            
+           OnCurrentValueChanged();
+       }
+      
+   }
+}
